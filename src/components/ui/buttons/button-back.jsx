ca47a10244da6e103a47f1be4e74f7a37fcea6b0
Rafacruiz <rafacruiz@gmail.com>
2026-01-21 21:28:45 +0100

    chore(ui): buttonBack add default prop value

--- src/components/ui/buttons/button-back.jsx
+++ src/components/ui/buttons/button-back.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 
-function ButtonBack () {
-    return (<Link className="btn btn-outline-dark w-100" to={-1}>Back</Link>);
+function ButtonBack ({to = -1}) {
+    return (<Link className="btn btn-outline-dark w-100" to={to}>Back</Link>);
 }
 
 export default ButtonBack;
