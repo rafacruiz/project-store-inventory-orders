34b9dae69034e807283cc3adad9da9b86ab97f1b
Rafacruiz <rafacruiz@gmail.com>
2026-01-25 21:55:23 +0100

    feat(ui): icon login form

--- src/pages/login-page.jsx
+++ src/pages/login-page.jsx
@@ -1,9 +1,17 @@
+import boxIcon from '../assets/img/icons/box-icon.png';
 import { LoginForm } from "../components/users";
 
 function LoginPage () {
     return (
         <div className="container d-flex justify-content-center align-items-center vh-100">
             <div className="col-12 col-md-6">
+                <div className="d-flex justify-content-center align-items-center py-3">
+                    <img 
+                        src={boxIcon} 
+                        class="rounded mx-auto d-block" 
+                        alt="ShopManager" 
+                        style={{ width: '60px', height: '60px' }} />
+                </div>
                 <div className="card shadow">
                     <div className="card-body p-4">
                         <LoginForm />
