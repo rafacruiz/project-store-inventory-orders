56c22045de698b1c2a7b9746a2c70b53b7b4186d
Rafacruiz <rafacruiz@gmail.com>
2026-01-31 10:12:47 +0100

    chore(ui): add search icon to finder input

--- src/components/ui/inputs/input-finder.jsx
+++ src/components/ui/inputs/input-finder.jsx
@@ -7,12 +7,18 @@ function InputFinder ({ onChange, inputOption }) {
     };
 
     return (
-        <input 
-            type="text" 
-            className="form-control"
-            id={`name-${inputOption?.id}-finder`}
-            placeholder={ inputOption?.placeholder } 
-            onChange={ handleChange } />
+
+        <div className="input-group">
+            <span className="input-group-text bg-white border-end-0">
+                <i className="fa fa-search text-muted"></i>
+            </span>
+            <input 
+                type="text" 
+                className="form-control border-start-0"
+                id={`name-${inputOption?.id}-finder`}
+                placeholder={ inputOption?.placeholder } 
+                onChange={ handleChange } />
+        </div>
     );
 }
 
