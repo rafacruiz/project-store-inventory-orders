8481b41bf5cf71a372f4f85e4c98091bb4ce2962
Rafacruiz <rafacruiz@gmail.com>
2026-02-02 15:55:31 +0100

    chore: Redesign 404 error page layout

--- src/pages/404-page.jsx
+++ src/pages/404-page.jsx
@@ -1,10 +1,10 @@
 
 function NotFoundPage () {
     return (
-        <div className="container d-flex flex-row justify-content-center my-auto gap-3">
+        <div className="container d-flex flex-row justify-content-center align-items-center my-auto gap-3">
             <h1 className="fw-bold m-0">404</h1>
             <div class="vr"></div>
-            <p className="m-0 d-flex align-items-center"><b>Oops!&nbsp;</b> The page you are looking for does not exist.</p>
+            <p className="m-0"><b>Oops!&nbsp;</b> The page you are looking for does not exist.</p>
         </div>
     );
 }
